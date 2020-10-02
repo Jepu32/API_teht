36691d7652dcc16a6e80f92ca5b1407b81963684
Jepu32 <jere.saarelma@metropolia.fi>
2020-10-02 19:06:55 +0300

    Updates

---
+++ precache-manifest.1049018781ff0eff08efa86f39dd261a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c9c5aac49b41cea0f25df6954fc1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a40dccb633da54177fde",
+    "url": "/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "38231414b235dc5bbf1f",
+    "url": "/static/css/main.ebd016bb.chunk.css"
+  },
+  {
+    "revision": "a40dccb633da54177fde",
+    "url": "/static/js/2.eb9fe44b.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/static/js/2.eb9fe44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38231414b235dc5bbf1f",
+    "url": "/static/js/main.4e356611.chunk.js"
+  },
+  {
+    "revision": "f7590ba6b615daf2f665",
+    "url": "/static/js/runtime-main.355aec6a.js"
+  }
+]);
